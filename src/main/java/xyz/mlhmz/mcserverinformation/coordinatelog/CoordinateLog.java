a3985a6b97893c0e9e3e31af6640d3ebb57ccd2c
Malek Hamza <malek.hamza@gmx.de>
2024-02-05 21:39:21 +0100

    Initial commit

---
+++ src/main/java/xyz/mlhmz/mcserverinformation/coordinatelog/CoordinateLog.java
@@ -0,0 +1,17 @@
+package xyz.mlhmz.mcserverinformation.coordinatelog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CoordinateLog extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
