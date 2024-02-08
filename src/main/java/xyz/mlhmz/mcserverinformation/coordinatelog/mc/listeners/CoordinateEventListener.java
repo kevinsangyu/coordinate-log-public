f344b153366fa7c95f9eb3a20dff50313091894f
Malek Hamza <malek.hamza@gmx.de>
2024-02-08 22:06:37 +0100

    Add entry addition on PlayerDeathEvent

---
+++ src/main/java/xyz/mlhmz/mcserverinformation/coordinatelog/mc/listeners/CoordinateEventListener.java
@@ -0,0 +1,21 @@
+package xyz.mlhmz.mcserverinformation.coordinatelog.mc.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import xyz.mlhmz.mcserverinformation.coordinatelog.CoordinateLog;
+import xyz.mlhmz.mcserverinformation.coordinatelog.entities.Entry;
+import xyz.mlhmz.mcserverinformation.coordinatelog.stores.EntryStore;
+
+public class CoordinateEventListener implements Listener {
+    EntryStore entryStore;
+
+    public CoordinateEventListener() {
+        entryStore = CoordinateLog.getInstance(EntryStore.class);
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        entryStore.saveEntry(new Entry("Died", event.getEntity().getUniqueId(), event.getEntity().getLocation()));
+    }
+}
